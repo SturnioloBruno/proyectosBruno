cbe96171bf9b2ee323ccb75d1d62d4d81c33060d
sturniolo <br1.sturniolo@gmail.com>
2023-03-28 11:58:23 -0300

    hilos sincronizados con Condition

--- UsoDeHilos/HilosBanco/src/main/java/org/example/Banco.java
+++ UsoDeHilos/HilosBanco/src/main/java/org/example/Banco.java
@@ -1,11 +1,13 @@
 package org.example;
 
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Banco {
     private final double[] cuentas;
     private Lock cierreBanco = new ReentrantLock();
+    private Condition saldoSuficiente;
 
     public Banco() {
         this.cuentas = new double[100];
@@ -13,19 +15,21 @@ public class Banco {
         for (int i = 0; i < cuentas.length  ; i++) {
             cuentas[i] = 2000;
         }
+        saldoSuficiente = cierreBanco.newCondition();
     }
 
-    public void transferencia (int cuentaOrigen, int cuentaDestino, double cantidad) {
+    public void transferencia (int cuentaOrigen, int cuentaDestino, double cantidad) throws InterruptedException {
         cierreBanco.lock();
         try {
-            if (cuentas[cuentaOrigen] < cantidad) {
-                return;
+            while (cuentas[cuentaOrigen] < cantidad) {
+                saldoSuficiente.await();
             }
             System.out.println(Thread.currentThread());
             cuentas[cuentaOrigen] -= cantidad;
             System.out.printf( "%10.2f de %d para %d" , cantidad, cuentaOrigen, cuentaDestino);
             cuentas[cuentaDestino] += cantidad;
             System.out.printf(" Saldo total: %10.2f%n", getSaldoTotal());
+            saldoSuficiente.signalAll();
         }finally {
             cierreBanco.unlock();
         }
