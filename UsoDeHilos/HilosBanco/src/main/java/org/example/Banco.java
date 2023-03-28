03004e98a597d934ed502bd2af63d7293734bfe4
sturniolo <br1.sturniolo@gmail.com>
2023-03-28 12:05:46 -0300

    synchronized

--- UsoDeHilos/HilosBanco/src/main/java/org/example/Banco.java
+++ UsoDeHilos/HilosBanco/src/main/java/org/example/Banco.java
@@ -6,8 +6,8 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Banco {
     private final double[] cuentas;
-    private Lock cierreBanco = new ReentrantLock();
-    private Condition saldoSuficiente;
+    //private Lock cierreBanco = new ReentrantLock();
+    //private Condition saldoSuficiente;
 
     public Banco() {
         this.cuentas = new double[100];
@@ -15,24 +15,26 @@ public class Banco {
         for (int i = 0; i < cuentas.length  ; i++) {
             cuentas[i] = 2000;
         }
-        saldoSuficiente = cierreBanco.newCondition();
+        //saldoSuficiente = cierreBanco.newCondition();
     }
 
-    public void transferencia (int cuentaOrigen, int cuentaDestino, double cantidad) throws InterruptedException {
-        cierreBanco.lock();
-        try {
+    public synchronized void transferencia (int cuentaOrigen, int cuentaDestino, double cantidad) throws InterruptedException {
+        //cierreBanco.lock();
+        //try {
             while (cuentas[cuentaOrigen] < cantidad) {
-                saldoSuficiente.await();
+                //saldoSuficiente.await();
+                wait();
             }
             System.out.println(Thread.currentThread());
             cuentas[cuentaOrigen] -= cantidad;
             System.out.printf( "%10.2f de %d para %d" , cantidad, cuentaOrigen, cuentaDestino);
             cuentas[cuentaDestino] += cantidad;
             System.out.printf(" Saldo total: %10.2f%n", getSaldoTotal());
-            saldoSuficiente.signalAll();
-        }finally {
-            cierreBanco.unlock();
-        }
+            //saldoSuficiente.signalAll();
+            notifyAll();
+//        }finally {
+//            cierreBanco.unlock();
+//        }
 
     }
 
