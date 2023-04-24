44c4e82532105331280c10f3bc46bb0537cfa521
sturniolo <br1.sturniolo@gmail.com>
2023-04-24 17:32:34 -0300

    introduccion syncronizhed

--- UsoDeHilos/cursojava/src/Main.java
+++ UsoDeHilos/cursojava/src/Main.java
@@ -1,48 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) {
-        // Streams /Lambda
+    public static void main(String[] args) throws IOException {
+        // Download webs
+        List<String> links = new ArrayList<>();
+        links.add("https://www.bbc.com");
+        links.add("https://www.bbc.com/news/uk-611196071");
+        links.add("https://www.bbc.com/news/world-africa-65374093");
+        links.add("https://www.bbc.com/news/world-us-canada-65379340");
+        links.add("https://www.bbc.com/news/world-middle-east-65374749");
 
-        List<String> cities = new ArrayList<>();
-        cities.add("London");
-        cities.add("New York");
-        cities.add("Tokyo");
-        cities.add("Barcelona");
-        cities.add("Buenos Aires");
-        cities.add("Bogota");
-        cities.add("Ciudad de México");
-        cities.add("Lima");
+        links.stream().parallel().forEach(link -> getWebContent(link));
 
-        for (int i = 0 ; i < cities.size() ; i++) {
-            System.out.println(cities.get(i));
-        }
-        System.out.println("Hello world!");
 
-        for (String city :
-                cities) {
-            System.out.println(city);
-        }
 
-        cities.stream().forEach(city -> System.out.println(city));
-        cities.stream().forEach(Main::printCity);
+        String result = getWebContent(links.get(0));
 
-        cities.stream().forEach(System.out::println);
-        cities.forEach(System.out::println);
-
-        cities.stream().parallel().forEach(System.out::println);
-        cities.stream().filter(city -> city.startsWith("B")).forEach(System.out::println);
-
-        cities.stream().filter(city -> city.startsWith("B"))
-                .filter(city -> city.contains("n"))
-                .forEach(System.out::println);
+        System.out.println(result);
     }
 
-    public static void printCity(String city) {
-        System.out.println(city);
-    }
-    public static boolean filterCity(String city) {
-        return city.startsWith("B");
+    private synchronized static String getWebContent(String link){
+        System.out.println("INIT");
+        System.out.println(link);
+        try {
+            URL url = new URL(link);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            String encoding = conn.getContentEncoding();
+
+            InputStream input = conn.getInputStream();
+            System.out.println("END");
+            return new BufferedReader(new InputStreamReader(input))
+                    .lines().collect(Collectors.joining());
+        } catch (MalformedURLException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return "";
     }
 }
